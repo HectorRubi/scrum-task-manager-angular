01046f6d64fae85a934f98de7b0f67d839693d98
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2023-03-06 15:40:49 -0600

    Create not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TaskViewComponent
+    TaskViewComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
