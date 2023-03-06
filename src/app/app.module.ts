0f78a863da2c1dc8a804e1be6170cd4d2d0a3de0
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2023-03-06 15:43:53 -0600

    Create layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     TaskViewComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
