5c6fe579edb66c1ac3d46a57c74641f72a543de2
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2023-03-06 17:02:14 -0600

    Add initial structure and design to tasks container

--- src/app/components/tasks-container/tasks-container.component.ts
+++ src/app/components/tasks-container/tasks-container.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Task } from 'src/app/models/task.model';
+import { TaskStatus } from 'src/app/utils/tasks';
 
 @Component({
   selector: 'app-tasks-container',
   templateUrl: './tasks-container.component.html',
   styleUrls: ['./tasks-container.component.css']
 })
-export class TasksContainerComponent {
+export class TasksContainerComponent implements OnInit {
+  @Input() title: TaskStatus = 'ready';
+  taskList: Task[] = [];
 
+  ngOnInit() {
+    this.taskList.push(new Task());
+    this.taskList.push(new Task());
+    this.taskList.push(new Task());
+    this.taskList.push(new Task());
+  }
 }
